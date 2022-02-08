b28f251a116d33c2e7f810c09db9f0121554aeb7
ArthurCost4 <arthurlinhares69@gmail.com>
2022-02-08 15:23:50 -0300

    update on PPT

--- js/PPT.js
+++ js/PPT.js
@@ -3,7 +3,8 @@ var escolha = prompt("Pedra papel TEESOURA: ");
 escolha = escolha.toUpperCase()
 console.log(escolha)
 */
-
+var placareu = 0;
+var placarai = 0;
 document.getElementById("saidaa").addEventListener("click", (ev)=>{
     ev.preventDefault()
     const tu = document.getElementById("tua")
@@ -11,31 +12,42 @@ document.getElementById("saidaa").addEventListener("click", (ev)=>{
     
     let PPT = ["PEDRA", "PAPEL", "TESOURA"]
     let comp = PPT[Math.floor(Math.random()* PPT.length)]
-    console.log(comp)
     
     if(value == comp){
         document.getElementById("saida").innerHTML = `VOCE USOU ${value.bold()} E EU ${comp.toUpperCase()}, OU SEJA, EMPATE`
     }
 
     if(value === "PEDRA" && comp == "PAPEL"){
-        document.getElementById("saida").innerHTML = `VOCE USOU ${value.bold()} E EU ${comp.toUpperCase()}, OU SEJA, PERDEEEU`
+        document.getElementById("saida").innerHTML = `VOCE USOU ${value.bold()} E EU ${comp.toUpperCase()}, OU SEJA, PERDEEEU`;
+        placarai++
+        document.getElementById("ai").innerHTML = placarai
     }
     if(value === "PEDRA" && comp == "TESOURA"){
         document.getElementById("saida").innerHTML = `VOCE USOU ${value.bold()} E EU ${comp.toUpperCase()}, OU SEJA, GANHOU`
+        placareu++
+        document.getElementById("eu").innerHTML = placareu
     }
 
     if(value === "PAPEL" && comp == "TESOURA"){
         document.getElementById("saida").innerHTML = `VOCE USOU ${value.bold()} E EU ${comp}, OU SEJA, PERDEEEU`
+        placarai++
+        document.getElementById("ai").innerHTML = placarai
     }
     if(value === "PAPEL" && comp == "PEDRA"){
         document.getElementById("saida").innerHTML = `VOCE USOU ${value.bold()} E EU ${comp}, OU SEJA, GANHOU`
+        placareu++
+        document.getElementById("eu").innerHTML = placareu
     }
 
     if(value === "TESOURA" && comp == "PEDRA"){
         document.getElementById("saida").innerHTML = `VOCE USOU ${value.bold()} E EU ${comp}, OU SEJA, PERDEEEU`
+        placarai++
+        document.getElementById("ai").innerHTML = placarai
     }
     if(value === "TESOURA" && comp == "PAPEL"){
         document.getElementById("saida").innerHTML = `VOCE USOU ${value.bold()} E EU ${comp}, OU SEJA, GANHOU`
+        placareu++
+        document.getElementById("eu").innerHTML = placareu
     }
 
 })
