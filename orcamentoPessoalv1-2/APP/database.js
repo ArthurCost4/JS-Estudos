daecd4a4288ad3c3fe7ee18e9763d600ae99f750
Arthur Costa <95101382+ArthurCost4@users.noreply.github.com>
2022-04-08 18:27:20 -0300

    Update database.js

--- orcamentoPessoalv1-2/APP/database.js
+++ orcamentoPessoalv1-2/APP/database.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2');
 function conectar(){
     const connection = mysql.createConnection({
         host:'localhost',
-        user: 'root',
-        password: '1234',
+        user: 'user',
+        password: 'pass',
         database: 'orcamentopessoal'
     });
     return connection
